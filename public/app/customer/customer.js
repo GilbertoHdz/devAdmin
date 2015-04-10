9429b05735ee0643b45ed965d85d782e25b2673a
Gilberto Hernández <killer.krazydevil@gmail.com>
2015-04-09 19:02:22 -0500

    Excel
    Se agrega export excel

--- public/app/customer/customer.js
+++ public/app/customer/customer.js
@@ -2,13 +2,34 @@
 
 angular.module("devAdminApp")
 
-.config(function($routeProvider){
+.config(function ($routeProvider){
   $routeProvider
-  .when("/customer", {
+    .when("/customer", {
       templateUrl: "/app/customer/customer.html",
       controller: "CustomerCtrl"
+    })
+    .when('/customer/excel', {
+        templateUrl: 'app/customer/customer.html',
+        controller: 'CustomerCtrl',
+        resolve: {
+          customer: function(services, $route){
+            var customerID = $route.current.params.customerID;
+              if(customerID != 0){
+                return services.getCustomer(customerID);
+              } else {return ;}
+          }
+        }
+    })
+    .otherwise({
+        redirectTo: '/'
     });
 })
-.controller("CustomerCtrl", function ($scope){
+.controller("CustomerCtrl",function ($scope, $http){
   $scope.message = "Customer Controller";
+
+ 	$scope.excel = function() {
+	    $http.get('/customers/excel');
+	};
+
+
 });
